7b43fe59701521e408e3018ea65418439a2b573d
sot528 <sot528@gmail.com>
2018-03-31 21:25:55 +0900

    update packages

--- test/helpers/alis_helper.js
+++ test/helpers/alis_helper.js
@@ -3,7 +3,7 @@ const chaiAsPromised = require('chai-as-promised');
 const chaiBigNumber = require('chai-bignumber');
 
 // exports
-export const BigNumber = web3.BigNumber;
+export const { BigNumber } = web3;
 export const should = chai
   .use(chaiAsPromised)
   .use(chaiBigNumber(BigNumber))
