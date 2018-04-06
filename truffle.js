22f411f612f0eed500eecbec3701354272d27691
sot528 <sot528@gmail.com>
2018-04-06 19:30:02 +0900

    improve truffle.js

--- truffle.js
+++ truffle.js
@@ -3,19 +3,13 @@ require('babel-polyfill');
 
 module.exports = {
   networks: {
-    live: {
-      network_id: 1,
-      host: "localhost",
-      port: 8545,
-      gas: 4400000,
-      gasPrice: 21000000000
-    },
     development: {
       host: "localhost",
       port: 8545,
       network_id: "*", // Match any network id
       gas: 3500000,
-      gasPrice: 2100000000000
+      gasPrice: 2100000000000,
+      from: '0x7ad8f90cfa071c8420e3f09fe0e413d0c47502e6'
     },
     test: {
       host: "localhost",
