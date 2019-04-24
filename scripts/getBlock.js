e10a0b5d089cd57401d597be4942c849cf6b8afb
sot528 <sot528@gmail.com>
2019-04-24 17:36:13 +0900

    refactoring

--- scripts/getBlock.js
+++ scripts/getBlock.js
@@ -1,16 +1,15 @@
 import { web3 } from './helper';
 
-const block_number = process.argv[2];
-if (!block_number) {
-  console.log("No block_number");
+const blockNumber = process.argv[2];
+if (!blockNumber) {
+  console.log('No block_number');
   process.exit();
 }
 
-const block = web3.eth.getBlock(block_number);
-console.log('Block number: ' + block.number);
-console.log('Time stamp: ' + block.timestamp);
+const block = web3.eth.getBlock(blockNumber);
+console.log(`Block number: ${block.number}`);
+console.log(`Time stamp: ${block.timestamp}`);
 
 const date = new Date(block.timestamp * 1000);
-console.log('Date: ' + date.getFullYear() + '/' + date.getMonth() + '/' + date.getDate() + ' ' +
-  date.getHours() + ':' + date.getMinutes() + ':' + date.getSeconds()
-);
+console.log(`Date: ${date.getFullYear()}/${date.getMonth()}/${date.getDate()} ${
+  date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`);
